1ce5e92f71e3970b7ee69ef47807860a07bf1f52
isif00 <sifodev@gmail.com>
2023-11-14 23:41:10 +0100

    fix bugs

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
 import { connectDB, connectServer } from "./config";
 import errorHandlerMiddleware from "./middlewares/errorHandling";
 import notFoundMiddleware from "./middlewares/notFound";
@@ -8,14 +11,17 @@ const app = express();
 
 // Middlewares
 app.use(express.json()); // To parse the incoming requests with JSON payloads
+app.use(cors());
+app.use(morgan("tiny"));
+
+// Routes
+app.use("/user", userRouter);
 
 // Config
 app.use(notFoundMiddleware); // For handling not found routes
 app.use(errorHandlerMiddleware()); // For handling errors
 
-// Routes
-app.use("/user", userRouter);
-
 // Connection
 connectDB(); // Connect to MongoDB
 connectServer(app); // Connect to Server
+
