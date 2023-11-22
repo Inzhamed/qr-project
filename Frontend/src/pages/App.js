9538b7988b87794e53e30ba74fb46853aeff6f1f
Inzhamed <95131724+Inzhamed@users.noreply.github.com>
2023-11-22 07:21:44 +0100

    Update App.js

--- Frontend/src/pages/App.js
+++ Frontend/src/pages/App.js
@@ -2,7 +2,6 @@ import "../styles/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import Qrcode from "./Qrcode";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" exact Component={SignIn} />
         <Route path="/signup" Component={SignUp} />
-        <Route path="/qrcode" Component={Qrcode} />
       </Routes>
     </Router>
   );
